d9b8e7815e48386e276e4ff3888d8cf24bafcab4
Daroch <daroch314@gmail.com>
2023-11-16 19:34:49 +0100

    adding style folder, SASS package and scss files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import PlantContainer from './components/plants/PlantContainer';
+import './style/main.scss';
 
 function App() {
   return (
-    <>
+    <div className='container'>
       <h1>Garden App</h1>
       <p>Gestiona tus plantas!!</p>
       <PlantContainer />
-    </>
+    </div>
   );
 }
 
