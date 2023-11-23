6adffa3352a4ea46896d2a43f2048f07466f34cf
Daroch <daroch314@gmail.com>
2023-11-23 13:38:49 +0100

    fix Navlink  active className

--- src/components/navigation/navigation-container.jsx
+++ src/components/navigation/navigation-container.jsx
@@ -9,32 +9,50 @@ const NavigationContainer = () => {
     <div className='nav-wrapper'>
       <div className='left-side'>
         <div className='nav-link-wrapper'>
-          <NavLink exact to='/' activeClassName='nav-link-active'>
+          <NavLink
+            to='/'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             Home
           </NavLink>
         </div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/plants' activeClassName='nav-link-active'>
+          <NavLink
+            to='/plants'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             My Plants
           </NavLink>
         </div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/alerts' activeClassName='nav-link-active'>
+          <NavLink
+            to='/alerts'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             Alerts
           </NavLink>
         </div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/explore' activeClassName='nav-link-active'>
+          <NavLink
+            to='/explore'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             Explore
           </NavLink>
         </div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/about' activeClassName='nav-link-active'>
+          <NavLink
+            to='/about'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             About
           </NavLink>
         </div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/contact' activeClassName='nav-link-active'>
+          <NavLink
+            to='/contact'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             Contact
           </NavLink>
         </div>
@@ -42,7 +60,10 @@ const NavigationContainer = () => {
       <div className='right-side'>
         <div>Daroch</div>
         <div className='nav-link-wrapper'>
-          <NavLink to='/login' activeClassName='nav-link-active'>
+          <NavLink
+            to='/login'
+            className={({ isActive }) => (isActive ? 'nav-link-active' : ' ')}
+          >
             Login
           </NavLink>
         </div>
