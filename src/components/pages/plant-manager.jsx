7eefa2633014bea11926164fc8f139a0f2aa808b
Daroch <daroch314@gmail.com>
2023-11-23 13:39:26 +0100

    adding showForm state variable

--- src/components/pages/plant-manager.jsx
+++ src/components/pages/plant-manager.jsx
@@ -1,19 +1,27 @@
+import { useState } from 'react';
 import PlantContainer from '../plants/PlantContainer';
 import PlantForm from '../plants/PlantForm';
 
 export default function PlantManager() {
+  const [showForm, setShowForm] = useState(false);
+
+  function LoadForm() {
+    if (showForm) {
+      return <PlantForm />;
+    }
+  }
   return (
     <div>
       <h1>Gestiona tus plantas!!</h1>
       <div
         className='btn'
         onClick={() => {
-          loadForm;
+          setShowForm(!showForm);
         }}
       >
-        Añadir planta
+        Mostrar/Ocultar - Añadir planta
+        <LoadForm />
       </div>
-      <PlantForm />
       <PlantContainer />
     </div>
   );
