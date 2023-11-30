71a6ad226d84f0fcdba578f2ecefe6c847263dad
Daroch <daroch314@gmail.com>
2023-11-30 03:32:41 +0100

    Creating alert Form, reorganice positions of fields and added logic

--- src/components/alerts/alert-form.jsx
+++ src/components/alerts/alert-form.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'https://cdn.skypack.dev/axios';
+
 import { fetchToken } from '../auth/login';
 
 export default function AlertForm({
@@ -8,23 +9,34 @@ export default function AlertForm({
   handleSuccessfulFormAlertEditSubmission,
   clearAlertToEdit,
   alertToEdit,
+  alertTypes,
+  plants,
 }) {
   const [alertData, setAlertData] = useState({
+    alert_type_id: 1,
     start_date: '',
+    title: '',
     status: true,
     repeat: true,
     frecuency: 3,
-    created_at: '',
-    plant_id: 1,
-    alert_type_id: 1,
+    plant_id: '',
     notes: '',
-    title: '',
   });
+  const [status, setStatus] = useState(true);
+  const [repeat, setRepeat] = useState(true);
   const [formParameters, setFormParameters] = useState({
     editMode: false,
-    apiUrl: 'http://localhost:8000/users/' + loggedUserId + '/addalert',
+    apiUrl: `http://localhost:8000/users/${loggedUserId}/plant/${alertData.plant_id}/addalert`,
     apiAction: 'post',
   });
+
+  const handleStatusChange = () => {
+    setStatus(!status);
+  };
+
+  const handleRepeatChange = () => {
+    setRepeat(!repeat);
+  };
 
   function handleChange(event) {
     setAlertData({
@@ -35,6 +47,8 @@ export default function AlertForm({
 
   function handleSubmit(event) {
     event.preventDefault();
+    alertData.repeat = repeat;
+    alertData.status = status;
     axios({
       method: formParameters.apiAction,
       url: formParameters.apiUrl,
@@ -68,39 +82,78 @@ export default function AlertForm({
       clearAlertToEdit();
       setFormParameters({
         editMode: true,
-        apiUrl: `http://localhost:8000/users/${loggedUserId}/updatealert/${alertToEdit.id}`,
+        apiUrl: `http://localhost:8000/users/${loggedUserId}/plants/${alertData.plant_id}/updatealert/${alertToEdit.id}`,
         apiAction: 'patch',
       });
     }
   }, [alertToEdit]);
+  useEffect(() => {
+    if (alertData.plant_id) {
+      setFormParameters({
+        editMode: false,
+        apiUrl: `http://localhost:8000/users/${loggedUserId}/plants/${alertData.plant_id}/addalert`,
+        apiAction: 'post',
+      });
+    }
+  }, [alertData.plant_id]);
 
   return (
     <form onSubmit={handleSubmit} className='plant-form-wrapper'>
       <div className='two-column'>
+        <select
+          name='alert_type_id'
+          id='alert_type_id'
+          placeholder='Tipo de Alerta'
+          value={alertData.alert_type_id}
+          onChange={handleChange}
+          className='select-element'
+        >
+          {alertTypes.map(alertType => {
+            return (
+              <option key={alertType.id} value={alertType.id}>
+                {alertType.alert_name}
+              </option>
+            );
+          })}
+        </select>
         <input
           type='text'
           name='start_date'
           id='start_date'
-          placeholder='Start Date'
+          placeholder='Fecha alerta'
           value={alertData.start_date}
           onChange={handleChange}
         />
+      </div>
+      <div className='four-column'>
+        Titulo
+        <input
+          type='text'
+          name='title'
+          id='title'
+          placeholder='Alert title'
+          value={alertData.title}
+          onChange={handleChange}
+        />
+        Activada:
         <input
           type='checkbox'
           name='status'
           id='status'
-          checked={alertData.status}
-          onChange={handleChange}
+          checked={status}
+          onChange={handleStatusChange}
         />
       </div>
-      <div className='two-column'>
+      <div className='four-column'>
+        Repetir:
         <input
           type='checkbox'
           name='repeat'
           id='repeat'
-          checked={alertData.repeat}
-          onChange={handleChange}
+          checked={repeat}
+          onChange={handleRepeatChange}
         />
+        Frecuencia:
         <input
           type='text'
           name='frecuency'
@@ -110,25 +163,8 @@ export default function AlertForm({
           onChange={handleChange}
         />
       </div>
-      <div className='two-column'>
-        <input
-          type='text'
-          name='created_at'
-          id='created_at'
-          placeholder='Created At'
-          value={alertData.created_at}
-          onChange={handleChange}
-        />
-        <input
-          type='number'
-          name='alert_type_id'
-          id='alert_type_id'
-          placeholder='Alert Type ID'
-          value={alertData.alert_type_id}
-          onChange={handleChange}
-        />
-      </div>
-      <div className='one-column'>
+      <div className='four-column'>
+        Notas:
         <input
           type='text'
           name='notes'
@@ -137,6 +173,22 @@ export default function AlertForm({
           value={alertData.notes}
           onChange={handleChange}
         />
+        Planta asociada:
+        <select
+          name='plant_id'
+          placeholder='Planta'
+          value={alertData.plant_id}
+          onChange={handleChange}
+          className='select-element'
+        >
+          {plants.map(plant => {
+            return (
+              <option key={plant.id} value={plant.id}>
+                {plant.name}
+              </option>
+            );
+          })}
+        </select>
       </div>
       <button className='btn' type='submit'>
         Submit
